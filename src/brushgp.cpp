eff2658ace93edc3f55ec6948b961ecdcb9f4fb6
Joe Romano <jdromano2@gmail.com>
2021-11-15 16:32:40 -0500

    Make Python interface build by fixing linker issue.

--- src/brushgp.cpp
+++ src/brushgp.cpp
@@ -16,9 +16,17 @@ license: GNU/GPL v3
 
 namespace py = pybind11;
 
+int add(int i, int j) {
+    return i + j;
+}
+
+
+
 PYBIND11_MODULE(brushgp, m) {
-    py::class_<Brush::Program<float> >(m, "Program")
-        .def(py::init(&Brush::Program<float>::create_py));
+    m.doc() = "pybind11 example";
+    m.def("add", &add, "A function that adds two numbers");
+    // py::class_<Brush::Program<float> >(m, "Program")
+    //     .def(py::init(&Brush::Program<float>::create_py));
         // .def("fit", &Brush::Program<float>::fit_py)
         // .def("predict", &Brush::Program<float>::predict_py)
         // .def("loss", &Brush::Program<float>::loss_py)
