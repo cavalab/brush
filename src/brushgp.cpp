790e30201b2a21f532304e157ab4e1ae1c83b667
Joe Romano <jdromano2@gmail.com>
2021-11-21 17:23:19 -0500

    Add Data, SearchSpace, and Program initializer Python bindings

--- src/brushgp.cpp
+++ src/brushgp.cpp
@@ -1,40 +1,48 @@
 /* Brush
 
-brushgp.cc : Python interface to brush classes and functions, using pybind11
+brushgp.cc : Python interface to Brush classes and functions, using pybind11
 
-copyright 2020 William La Cava
+copyright 2021 William La Cava
 authors: William La Cava and Joseph D. Romano
 license: GNU/GPL v3
 */
 
 #include <pybind11/pybind11.h>
+#include <pybind11/eigen.h>
 
 #include "program.h"
+#include "search_space.h"
+#include "data/data.h"
 
 #define STRINGIFY(x) #x
 #define MACRO_STRINGIFY(x) STRINGIFY(x)
 
 namespace py = pybind11;
 
-int add(int i, int j) {
-    return i + j;
-}
-
-
 
 PYBIND11_MODULE(brushgp, m) {
-    m.doc() = "pybind11 example";
-    m.def("add", &add, "A function that adds two numbers");
-    // py::class_<Brush::Program<float> >(m, "Program")
-    //     .def(py::init(&Brush::Program<float>::create_py));
-        // .def("fit", &Brush::Program<float>::fit_py)
-        // .def("predict", &Brush::Program<float>::predict_py)
-        // .def("loss", &Brush::Program<float>::loss_py)
-        // .def("d_loss", &Brush::Program<float>::d_loss_py)
+    m.doc() = "Python interface for Brush";
 
 #ifdef VERSION_INFO
     m.attr("__version__") = MACRO_STRINGIFY(VERSION_INFO);
 #else
     m.attr("__version__") = "dev";
 #endif
+
+    py::class_<Brush::data::Data>(m, "Data")
+        .def(py::init<ArrayXXf, ArrayXf>());
+
+    // Notice: We change the interface for SearchSpace a little bit by 
+    // constructing it with a Data object, rather than initializing it as an
+    // empty struct and then calling init() with the Data object.
+    py::class_<Brush::SearchSpace>(m, "SearchSpace")
+        .def(py::init([](Brush::data::Data data) {
+            SearchSpace SS;
+            SS.init(data);
+            return SS;
+        }));
+
+    py::class_<Brush::Program<ArrayXf> >(m, "Program")
+        .def(py::init<SearchSpace&, int, int, int>())
+        .def("fit", &Brush::Program<ArrayXf>::fit);
 }
