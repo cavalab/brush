920996da6149f3aad4a7f2f4ea5e89a21dd89934
gAldeia <guilherme.seidyo@gmail.com>
2023-04-13 18:09:02 -0300

    Create tests to check max_size after variation

--- tests/cpp/test_program.cpp
+++ tests/cpp/test_program.cpp
@@ -111,9 +111,6 @@ TEST(Program, FitClassifier)
     }
 }
 
-
-
-
 TEST(Program, Serialization)
 {
     // test mutation
@@ -160,6 +157,43 @@ TEST(Program, Serialization)
             newPRG.fit(data);
             ArrayXf new_y_pred = newPRG.predict(data);
 
+        }
+    }
+}
+
+TEST(Operators, ProgramMaxSizePARAMS)
+{
+    MatrixXf X(10,2);
+    ArrayXf y(10);
+    X << 0.85595296, 0.55417453, 0.8641915 , 0.99481109, 0.99123376,
+         0.9742618 , 0.70894019, 0.94940306, 0.99748867, 0.54205151,
+
+         0.5170537 , 0.8324005 , 0.50316305, 0.10173936, 0.13211973,
+         0.2254195 , 0.70526861, 0.31406024, 0.07082619, 0.84034526;
+
+    y << 3.55634251, 3.13854087, 3.55887523, 3.29462895, 3.33443517,
+         3.4378868 , 3.41092345, 3.5087468 , 3.25110243, 3.11382179;
+
+    Dataset data(X,y);
+
+    SearchSpace SS;
+    SS.init(data);
+
+    for (int d = 1; d < 10; ++d)
+    {
+        for (int s = 1; s < 10; ++s)
+        {
+            PARAMS["max_size"]  = s;
+            PARAMS["max_depth"] = d;
+
+            fmt::print("d={},s={}\n",d,s);
+            fmt::print("make_regressor\n");
+            RegressorProgram PRG = SS.make_regressor(0, 0);
+            
+            auto PRG_model = PRG.get_model("compact", true);
+
+            ASSERT_TRUE(PRG.size() > 0);
+            ASSERT_TRUE(PRG.size() <= s);
         }
     }
 }
--- tests/cpp/test_variation.cpp
+++ tests/cpp/test_variation.cpp
@@ -8,7 +8,7 @@ TEST(Operators, Mutation)
 {
     PARAMS["mutation_options"] = {
         {"point",0.25}, {"insert", 0.25}, {"delete", 0.25}, {"toggle_weight", 0.25}
-        };
+    };
     // test mutation
     // TODO: set random seed
     MatrixXf X(10,2);
@@ -59,14 +59,18 @@ TEST(Operators, Crossover)
 {
     // test mutation
     // TODO: set random seed
+
     MatrixXf X(10,2);
     ArrayXf y(10);
     X << 0.85595296, 0.55417453, 0.8641915 , 0.99481109, 0.99123376,
          0.9742618 , 0.70894019, 0.94940306, 0.99748867, 0.54205151,
+
          0.5170537 , 0.8324005 , 0.50316305, 0.10173936, 0.13211973,
          0.2254195 , 0.70526861, 0.31406024, 0.07082619, 0.84034526;
+
     y << 3.55634251, 3.13854087, 3.55887523, 3.29462895, 3.33443517,
-             3.4378868 , 3.41092345, 3.5087468 , 3.25110243, 3.11382179;
+         3.4378868 , 3.41092345, 3.5087468 , 3.25110243, 3.11382179;
+
     Dataset data(X,y);
 
     SearchSpace SS;
@@ -114,6 +118,223 @@ TEST(Operators, Crossover)
             Child2.fit(data);
             auto child_pred1 = Child1.predict(data);
             auto child_pred2 = Child2.predict(data);
+        }
+    }
+}
+
+TEST(Operators, MutationMaxSizeLimit)
+{
+    PARAMS["mutation_options"] = {
+        {"point",0.25}, {"insert", 0.25}, {"delete", 0.25}, {"toggle_weight", 0.25}
+    };
+        
+    MatrixXf X(10,2);
+    ArrayXf y(10);
+    X << 0.85595296, 0.55417453, 0.8641915 , 0.99481109, 0.99123376,
+         0.9742618 , 0.70894019, 0.94940306, 0.99748867, 0.54205151,
+
+         0.5170537 , 0.8324005 , 0.50316305, 0.10173936, 0.13211973,
+         0.2254195 , 0.70526861, 0.31406024, 0.07082619, 0.84034526;
+
+    y << 3.55634251, 3.13854087, 3.55887523, 3.29462895, 3.33443517,
+         3.4378868 , 3.41092345, 3.5087468 , 3.25110243, 3.11382179;
+
+    Dataset data(X,y);
+
+    SearchSpace SS;
+    SS.init(data);
+
+    for (int d = 1; d < 10; ++d)
+    {
+        for (int s = 1; s < 10; ++s)
+        {
+            fmt::print("d={},s={}\n",d,s);
+            fmt::print("make_regressor\n");
+            RegressorProgram PRG = SS.make_regressor(d, s);
+            
+            auto PRG_model = PRG.get_model("compact", true);
+
+            auto Child = PRG.mutate();
+
+            fmt::print("print\n");
+            fmt::print(
+                "=================================================\n"
+                "depth = {}, size= {}\n"
+                "Initial Model: {}\n"
+                "Mutated Model: {}\n",
+                d, s, 
+                PRG.get_model("compact", true),
+                Child.get_model("compact", true)
+            );
+
+            // Original didn't change
+            ASSERT_TRUE(PRG_model == PRG.get_model("compact", true));
+            
+            // Child is within restrictions
+            ASSERT_TRUE(Child.size() > 0);
+            ASSERT_TRUE(Child.size() <= s);
+        }
+    }
+}
+
+TEST(Operators, CrossoverMaxSizeLimit)
+{
+    MatrixXf X(10,2);
+    ArrayXf y(10);
+    X << 0.85595296, 0.55417453, 0.8641915 , 0.99481109, 0.99123376,
+         0.9742618 , 0.70894019, 0.94940306, 0.99748867, 0.54205151,
+
+         0.5170537 , 0.8324005 , 0.50316305, 0.10173936, 0.13211973,
+         0.2254195 , 0.70526861, 0.31406024, 0.07082619, 0.84034526;
+
+    y << 3.55634251, 3.13854087, 3.55887523, 3.29462895, 3.33443517,
+         3.4378868 , 3.41092345, 3.5087468 , 3.25110243, 3.11382179;
+
+    Dataset data(X,y);
+
+    SearchSpace SS;
+    SS.init(data);
+
+    for (int d = 1; d < 10; ++d)
+    {
+        for (int s = 1; s < 10; ++s)
+        {
+            RegressorProgram PRG1 = SS.make_regressor(d, s);
+            RegressorProgram PRG2 = SS.make_regressor(d, s);
+
+            auto PRG1_model = PRG1.get_model("compact", true);
+            auto PRG2_model = PRG2.get_model("compact", true);
+
+            fmt::print(
+                "=================================================\n"
+                "depth = {}, size= {}\n"
+                "Initial Model 1: {}\n"
+                "Initial Model 2: {}\n",
+                d, s, 
+                PRG1.get_model("compact", true),
+                PRG2.get_model("compact", true)
+            );
+
+            fmt::print("cross one\n");
+            auto Child1 = PRG1.cross(PRG2);
+            fmt::print(
+                "Model 1 after cross: {}\n"
+                "Model 2 after cross: {}\n",
+                PRG1.get_model("compact", true),
+                PRG2.get_model("compact", true)
+            );
+
+            fmt::print("cross two\n");
+            auto Child2 = PRG2.cross(PRG1);
+            fmt::print(
+                "Crossed Model 1: {}\n"
+                "Crossed Model 2: {}\n"
+                "=================================================\n",
+                Child1.get_model("compact", true),
+                Child2.get_model("compact", true)
+            );
+
+            // Original didn't change
+            ASSERT_TRUE(PRG1_model == PRG1.get_model("compact", true));
+            ASSERT_TRUE(PRG2_model == PRG2.get_model("compact", true));
+
+            // Child1 is within restrictions
+            ASSERT_TRUE(Child1.size() > 0);
+            ASSERT_TRUE(Child1.size() <= s);
+
+            // Child2 is within restrictions
+            ASSERT_TRUE(Child2.size() > 0);
+            ASSERT_TRUE(Child2.size() <= s);
+        }
+    }
+}
+
+TEST(Operators, MutationMaxSizePARAMS)
+{
+    PARAMS["mutation_options"] = {
+        {"point",0.25}, {"insert", 0.25}, {"delete", 0.25}, {"toggle_weight", 0.25}
+    };
+        
+    MatrixXf X(10,2);
+    ArrayXf y(10);
+    X << 0.85595296, 0.55417453, 0.8641915 , 0.99481109, 0.99123376,
+         0.9742618 , 0.70894019, 0.94940306, 0.99748867, 0.54205151,
+
+         0.5170537 , 0.8324005 , 0.50316305, 0.10173936, 0.13211973,
+         0.2254195 , 0.70526861, 0.31406024, 0.07082619, 0.84034526;
+
+    y << 3.55634251, 3.13854087, 3.55887523, 3.29462895, 3.33443517,
+         3.4378868 , 3.41092345, 3.5087468 , 3.25110243, 3.11382179;
+
+    Dataset data(X,y);
+
+    SearchSpace SS;
+    SS.init(data);
+
+    for (int d = 1; d < 10; ++d)
+    {
+        for (int s = 1; s < 10; ++s)
+        {
+            PARAMS["max_size"]  = s;
+            PARAMS["max_depth"] = d;
+
+
+            fmt::print("d={},s={}\n",d,s);
+            fmt::print("make_regressor\n");
+            RegressorProgram PRG = SS.make_regressor(0, 0);
+            
+            auto PRG_model = PRG.get_model("compact", true);
+
+            auto Child = PRG.mutate();
+
+            // Child is within restrictions
+            ASSERT_TRUE(Child.size() > 0);
+            ASSERT_TRUE(Child.size() <= s);
+        }
+    }
+}
+
+TEST(Operators, CrossoverMaxSizePARAMS)
+{
+    MatrixXf X(10,2);
+    ArrayXf y(10);
+    X << 0.85595296, 0.55417453, 0.8641915 , 0.99481109, 0.99123376,
+         0.9742618 , 0.70894019, 0.94940306, 0.99748867, 0.54205151,
+
+         0.5170537 , 0.8324005 , 0.50316305, 0.10173936, 0.13211973,
+         0.2254195 , 0.70526861, 0.31406024, 0.07082619, 0.84034526;
+
+    y << 3.55634251, 3.13854087, 3.55887523, 3.29462895, 3.33443517,
+         3.4378868 , 3.41092345, 3.5087468 , 3.25110243, 3.11382179;
+
+    Dataset data(X,y);
+
+    SearchSpace SS;
+    SS.init(data);
+
+    for (int d = 1; d < 10; ++d)
+    {
+        for (int s = 1; s < 10; ++s)
+        {
+            PARAMS["max_size"]  = s;
+            PARAMS["max_depth"] = d;
+
+            RegressorProgram PRG1 = SS.make_regressor(d, s);
+            RegressorProgram PRG2 = SS.make_regressor(d, s);
+
+            auto PRG1_model = PRG1.get_model("compact", true);
+            auto PRG2_model = PRG2.get_model("compact", true);
+
+            auto Child1 = PRG1.cross(PRG2);
+            auto Child2 = PRG2.cross(PRG1);
+
+            // Child1 is within restrictions
+            ASSERT_TRUE(Child1.size() > 0);
+            ASSERT_TRUE(Child1.size() <= s);
+
+            // Child2 is within restrictions
+            ASSERT_TRUE(Child2.size() > 0);
+            ASSERT_TRUE(Child2.size() <= s);
         }
     }
 }
