129576f62188ac2786aa2099c107781763effa47
gAldeia <guilherme.seidyo@gmail.com>
2023-04-25 16:08:15 -0300

    Add constant weight optimizer test

--- tests/cpp/test_optimization.cpp
+++ tests/cpp/test_optimization.cpp
@@ -775,4 +775,76 @@ TEST(Program, Optimize3aryProductOuterWeight)
     ASSERT_TRUE(data.y.isApprox(y_pred, 1e-4));
     ASSERT_TRUE(learned_weights.isApprox(true_weights, 1e-4));
     ASSERT_TRUE(mse <= 1e-4);
-} 
+}
+
+TEST(Program, OptimizeConstant)
+{
+    /* @brief Tests whether weight optimization works on a simple multiplicative problem.
+        The dataset models y = 2*x1 * 3*x2. 
+        The initial model is yhat = 1*x1 * x2. 
+        The test checks that the target output and initial model are close, and also
+        whether the weights are correct. Given that the model yhat has the correct
+        structure, the fitted model should have an infinitesimally small error.
+    */
+    Dataset data = Data::read_csv("docs/examples/datasets/d_2x1_multiply_3x2.csv","target");
+    
+    SearchSpace SS;
+    SS.init(data);
+
+    json PRGjson = {
+        {"Tree", {
+        {
+            {"node_type","Mul"},
+            {"is_weighted", false}
+        },
+        {
+            {"node_type","Terminal"},
+            {"feature","x1"},
+            {"is_weighted", false}
+        },
+        {
+            {"node_type","Mul"},
+            {"is_weighted", false}
+        },
+        {
+            {"node_type","Terminal"},
+            {"feature","x2"},
+            {"is_weighted", false}
+        },
+        {
+            {"node_type","Constant"},
+            {"feature","C"},
+            {"is_weighted", true}
+        }
+        }},
+        {"is_fitted_",false}
+    };
+    fmt::print( "initial json: {}\n", PRGjson.dump(2));
+
+    // make program from json
+    RegressorProgram PRG = PRGjson;
+
+    // make json from the program just to visually check 
+    json loadedPRGjson = PRG;
+    fmt::print( "loaded json: {}\n", loadedPRGjson.dump(2));
+
+    // fit model
+    fmt::print( "fit\n");
+    PRG.fit(data);
+    fmt::print( "predict\n");
+    ArrayXf y_pred = PRG.predict(data);
+
+    auto learned_weights = PRG.get_weights();
+
+    std::cout << learned_weights << std::endl;
+
+    ArrayXf true_weights(1);
+    true_weights << 6.0;
+
+    // calculating the MSE
+    float mse = (data.y - y_pred).square().mean();
+
+    ASSERT_TRUE(data.y.isApprox(y_pred, 1e-4));
+    ASSERT_TRUE(learned_weights.isApprox(true_weights, 1e-4));
+    ASSERT_TRUE(mse <= 1e-4);
+}
