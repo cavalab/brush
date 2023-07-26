fb925a168d6a62b02a3e8652228e775b585c8db6
Guilherme Aldeia <guilherme.seidyo@gmail.com>
2023-07-26 14:16:22 -0400

    Initializing weights of floating number terminals

--- src/search_space.cpp
+++ src/search_space.cpp
@@ -4,6 +4,35 @@
 
 namespace Brush{
 
+
+float calc_initial_weight(const ArrayXf& value, const ArrayXf& y)
+{
+    // weights are initialized as the slope of the z-score of x and y.
+
+    // If y has different length from X, we get a core dump here. 
+    // TODO: need to make SS (or Datasaet) check for this when loading the data
+
+    vector<char> dtypes = {'f', 'f'};
+
+    MatrixXf data(value.size(), 2);
+
+    data.col(0) << value;
+    data.col(1) << y;
+
+    Normalizer n(true);
+    n.fit_normalize(data, dtypes); // normalize works row-wise
+
+    // In slope function, argument order matters (if not normalized with z-score)
+    // The feature should be the first value, and the true value the second
+    // (it will divide covar(arg1, arg2) by var(arg2)).
+    // Since z-score normalizes so mean=0 and std=1, then order doesnt matter
+    float prob_change = std::abs(slope(data.col(0).array(),   // x
+                                       data.col(1).array())); // y
+
+    return prob_change;
+}
+
+
 /// @brief generate terminals from the dataset features and random constants.
 /// @param d a dataset
 /// @return a vector of nodes 
@@ -19,21 +48,40 @@ vector<Node> generate_terminals(const Dataset& d)
                 using Scalar = typename T::Scalar;
                 constexpr bool weighted = std::is_same_v<Scalar, float>;
                 // if constexpr (T::Scalar == float)
-                terminals.push_back(Node(
+                auto n = Node(
                     NodeType::Terminal, 
                     Signature<T()>{}, 
                     weighted,
                     feature_name
-                ));
+                );
+
+                float prob_change = 1.0;
+                
+                // if the value can be casted to float array, we can calculate slope
+                if (std::holds_alternative<ArrayXf>(value)) {
+                    prob_change = calc_initial_weight(std::get<ArrayXf>(value), d.y);
+                }
+
+                n.set_prob_change( prob_change );
+
+                terminals.push_back(n);
             },
             value
         );
         ++i;
     };
 
-    // add a constant
-    terminals.push_back( Node(NodeType::Constant, Signature<ArrayXf()>{}, true, "C"));
-    terminals.push_back( Node(NodeType::Constant, Signature<ArrayXi()>{}, true, "C"));
+    // add constants
+    float num_const_prob_change = calc_initial_weight(VectorXf::Ones(d.y.size()), d.y);
+
+    auto cXf = Node(NodeType::Constant, Signature<ArrayXf()>{}, true, "C");
+    // cXf.set_prob_change(num_const_prob_change);
+    terminals.push_back(cXf);
+
+    auto cXi = Node(NodeType::Constant, Signature<ArrayXi()>{}, true, "C");
+    // cXi.set_prob_change(num_const_prob_change);
+    terminals.push_back(cXi);
+
     terminals.push_back( Node(NodeType::Constant, Signature<ArrayXb()>{}, false, "C"));
 
     return terminals;
@@ -77,7 +125,7 @@ void SearchSpace::init(const Dataset& d, const unordered_map<string,float>& user
 
         /* fmt::print("terminal ret_type: {}\n", DataTypeName[term.ret_type]); */
         terminal_map[term.ret_type].push_back(term);
-        terminal_weights[term.ret_type].push_back(1.0);
+        terminal_weights[term.ret_type].push_back(term.get_prob_change());
     }
 };
 
