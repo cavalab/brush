a564896995748226344015de14e53f6dfa7506b5
Guilherme Aldeia <guilherme.seidyo@gmail.com>
2023-08-05 15:30:50 -0400

    Updated tests after new mutation

--- tests/python/test_brush.py
+++ tests/python/test_brush.py
@@ -61,7 +61,7 @@ def test_fit(setup, brush_args, request):
         logging.error(traceback.format_exc())
         
 
-# def test_random_state():
+# def test_random_state(): # TODO: make it work
 #     test_y = np.array( [1. , 0. , 1.4, 1. , 0. , 1. , 1. , 0. , 0. , 0.  ])
 #     test_X = np.array([[1.1, 2.0, 3.0, 4.0, 5.0, 6.5, 7.0, 8.0, 9.0, 10.0],
 #                        [2.0, 1.2, 6.0, 4.0, 5.0, 8.0, 7.0, 5.0, 9.0, 10.0]]).T
--- tests/python/test_params.py
+++ tests/python/test_params.py
@@ -60,11 +60,12 @@ def _change_and_wait(config):
         'mutation_options': {'point'        : 0.0,
                              'insert'       : 0.0,
                              'delete'       : 0.0,
+                             'subtree'      : 0.0,
                              'toggle_weight': 0.0}
     }
 
     # We need to guarantee order to use the index correctly
-    mutations = ['point', 'insert', 'delete', 'toggle_weight']
+    mutations = ['point', 'insert', 'delete', 'subtree', 'toggle_weight']
 
     for i, m in enumerate(mutations):
         params['mutation_options'][m] = 0 if i != index else 1.0
