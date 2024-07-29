82133eab67b8e38f1b99f6551890e06811313bb5
gAldeia <guilherme.seidyo@gmail.com>
2024-07-29 13:24:11 -0300

    Docstrings

--- src/bandit/bandit.h
+++ src/bandit/bandit.h
@@ -15,9 +15,14 @@ namespace MAB {
 
 using namespace Brush;
 
-// TODO: add bandit to parameters and write doc in estimatorinterface
-
-// TODO: all templates, or require some specific types?
+/**
+ * @brief The Bandit struct represents a multi-armed bandit.
+ * 
+ * The Bandit struct encapsulates the bandit operator (policy) and provides\
+ * methods to set and get the arms, type, and probabilities of the bandit.
+ * It also provides a method to update the bandit's state based on the chosen
+ * arm and the received reward.
+ */
 template <typename T>
 struct Bandit
 {
@@ -28,30 +33,76 @@ public:
     std::shared_ptr<BanditOperator<T>> pbandit;
     // TODO: This should be a shared pointer to allow multiple instances of Bandit to share the same operator.
      
-    std::string type;
-    vector<T> arms;
+    std::string type; /**< The type of the bandit. */
+    vector<T> arms; /**< The arms of the bandit. */
 
-    std::map<T, float> probabilities;
+    std::map<T, float> probabilities; /**< The probabilities associated with each arm. */
 
     Bandit();
     ~Bandit(){};
 
+    /**
+     * @brief Constructor for the Bandit struct.
+     * @param type The type of the bandit.
+     * @param arms The arms of the bandit.
+     */
     Bandit(string type, vector<T> arms);
+
+    /**
+     * @brief Constructor for the Bandit struct.
+     * @param type The type of the bandit.
+     * @param arms_probs The arms and their associated probabilities.
+     */
     Bandit(string type, map<T, float> arms_probs);
 
+    /**
+     * @brief Sets the bandit operator (policy).
+     */
     void set_bandit();
 
+    /**
+     * @brief Sets the arms of the bandit.
+     */
     void set_arms(vector<T> arms);
+
+    /**
+     * @brief Gets the arms of the bandit.
+     */
     vector<T> get_arms();
 
+    /**
+     * @brief Gets the type of the bandit.
+     */
     string get_type();
+
+    /**
+     * @brief Sets the type of the bandit.
+     */
     void set_type(string type);
 
+    /**
+     * @brief Gets the probabilities associated with each arm.
+     */
     map<T, float> get_probs();
+
+    /**
+     * @brief Sets the probabilities associated with each arm.
+     * @param arms_probs The arms and their associated probabilities.
+     */
     void set_probs(map<T, float> arms_probs);
 
+    /**
+     * @brief Samples the probabilities associated with each arm using the policy.
+     * @param update Flag indicating whether to update the bandit's state, or just return the current probabilities.
+     * @return The sampled probabilities associated with each arm.
+     */
     map<T, float> sample_probs(bool update=false);
 
+    /**
+     * @brief Updates the bandit's state based on the chosen arm and the received reward.
+     * @param arm The chosen arm.
+     * @param reward The received reward.
+     */
     void update(T arm, float reward);
 };
 
--- src/bandit/bandit_operator.h
+++ src/bandit/bandit_operator.h
@@ -1,8 +1,6 @@
 #ifndef BANDIT_OPERATOR_H
 #define BANDIT_OPERATOR_H
 
-// virtual class. selection must be made with static methods
-
 #include "../init.h"
 #include "../data/data.h"
 #include "../types.h"
@@ -11,16 +9,47 @@
 namespace Brush {
 namespace MAB {
     
+/**
+ * @brief A **virtual** class representing a bandit operator.
+ * 
+ * This class provides functionality for sampling probabilities and updating rewards for different arms.
+ * 
+ * @tparam T The type of the arms.
+ */
 template<typename T>
 class BanditOperator
 {
 public:
+    /**
+     * @brief Constructs a BanditOperator object with a vector of arms.
+     * 
+     * @param arms The vector of arms.
+     */
     BanditOperator(vector<T> arms);
+    
+    /**
+     * @brief Constructs a BanditOperator object with a map of arms and their probabilities.
+     * 
+     * @param arms_probs The map of arms and their probabilities.
+     */
     BanditOperator(map<T, float> arms_probs);
+    
     ~BanditOperator() {};
 
+    /**
+     * @brief Samples the probabilities of the arms.
+     * 
+     * @param update A boolean indicating whether to update the probabilities.
+     * @return A map of arms and their probabilities.
+     */
     virtual std::map<T, float> sample_probs(bool update);
 
+    /**
+     * @brief Updates the reward for a specific arm.
+     * 
+     * @param arm The arm for which to update the reward.
+     * @param reward The reward value.
+     */
     virtual void update(T arm, float reward);
 protected:    
     std::map<T, float> probabilities;
