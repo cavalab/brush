dd7bbc482a59c1d8b75afb6a2ce80bb6f45b0c40
Guilherme Seidyo Imai Aldeia <guilherme.seidyo@gmail.com>
2026-04-22 17:08:59 -0400

    Removed old try catch to pick errors with scalar(w)

--- src/program/operator.h
+++ src/program/operator.h
@@ -29,53 +29,33 @@ namespace util{
                 }
             }
 
-            try //TODO: remove this try catch after debugging it
-            {
-                w = Scalar(tn.data.W);
-            }
-            catch (const std::exception& e) {
-                std::string err_msg = "Null pointer dereference: *weights is nullptr. "
-                                        "TreeNode ret_type: " + std::to_string(static_cast<int>(tn.data.ret_type)) +
-                                        ", name: " + tn.data.name;
-                std::cerr << "[EXCEPTION] get_weight: caught std::exception: " << e.what() << err_msg << std::endl;
-                throw;  // Re-throw to allow crash
-            } 
+            w = Scalar(tn.data.W);
         }
         else
         {
-            try //TODO: remove this try catch after debugging it
-            {
-                if (*weights == nullptr) {
-                    std::string err_msg = "Null pointer dereference: *weights is nullptr. "
-                                        "TreeNode ret_type: " + std::to_string(static_cast<int>(tn.data.ret_type)) +
-                                        ", name: " + tn.data.name;
-                    HANDLE_ERROR_THROW("Null pointer dereference: *weights is nullptr. " + err_msg);
-                }
-
-                // NLS case 1: floating point weight is stored in weights
-                if constexpr (is_same_v<Scalar, W>) 
-                    w = **weights;
-
-                // NLS case 2: a Jet/Dual weight is stored in weights, but this constant is a 
-                // integer type. We need to do some casting
-                else if constexpr (is_same_v<Scalar, iJet> && is_same_v<W, fJet>) {
-                    using WScalar = typename Scalar::Scalar;
-                    WScalar tmp = WScalar((**weights).a);    
-                    w = Scalar(tmp);
-                }
-                // NLS case 3: a Jet/Dual weight is stored in weights, matching Scalar type
-                else            
-                    w = Scalar(**weights);
-
-                *weights = *weights+1;
-            }
-            catch (const std::exception& e) {
+            if (*weights == nullptr) {
                 std::string err_msg = "Null pointer dereference: *weights is nullptr. "
-                                        "TreeNode ret_type: " + std::to_string(static_cast<int>(tn.data.ret_type)) +
-                                        ", name: " + tn.data.name;
-                std::cerr << "[EXCEPTION] get_weight: caught std::exception: " << e.what() << err_msg << std::endl;
-                throw;  // Re-throw to allow crash
-            }            
+                                    "TreeNode ret_type: " + std::to_string(static_cast<int>(tn.data.ret_type)) +
+                                    ", name: " + tn.data.name;
+                HANDLE_ERROR_THROW("Null pointer dereference: *weights is nullptr. " + err_msg);
+            }
+
+            // NLS case 1: floating point weight is stored in weights
+            if constexpr (is_same_v<Scalar, W>) 
+                w = **weights;
+
+            // NLS case 2: a Jet/Dual weight is stored in weights, but this constant is a 
+            // integer type. We need to do some casting
+            else if constexpr (is_same_v<Scalar, iJet> && is_same_v<W, fJet>) {
+                using WScalar = typename Scalar::Scalar;
+                WScalar tmp = WScalar((**weights).a);    
+                w = Scalar(tmp);
+            }
+            // NLS case 3: a Jet/Dual weight is stored in weights, matching Scalar type
+            else            
+                w = Scalar(**weights);
+
+            *weights = *weights+1;
         }
         return w;
     };
