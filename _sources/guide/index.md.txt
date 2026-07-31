82889776b6a66e9dcf6f5ffc677dd9319014b9bf
gAldeia <gAldeia@users.noreply.github.com>
2026-07-31 17:09:23 +0000

    deploy: 33a7db448efd7ece7f35561276ebaca29134da0c

--- _sources/guide/index.md.txt
+++ _sources/guide/index.md.txt
@@ -14,6 +14,7 @@ search_space
 working_with_programs
 json
 saving_loading_populations
+locking_mechanism
 archive
 deap
 ```
--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '0.1.dev1+gd8775497f',
+    VERSION: '0.1.dev1+g33a7db448',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
